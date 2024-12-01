8bedcc5c9c8ef2c6b627431b31694bcdd8d3218c
Sourabh thapliyal <75442117+Sourabh745@users.noreply.github.com>
2024-12-01 12:36:03 +0530

    js complete

---
+++ ChaiJS/JSFun/test.js
@@ -0,0 +1,9 @@
+
+// let fruits = [1,2,3]
+// fruits[5] = "mango";
+// fruits.length = 10;
+// console.log(fruits); // ['banana', 'apple', 'peach', empty x 2, 'mango', empty x 4]
+// console.log(Object.keys(fruits)); // ['0', '1', '2', '5']
+// console.log(fruits.length); // 10
+// console.log(fruits[8]); // undefined
+
